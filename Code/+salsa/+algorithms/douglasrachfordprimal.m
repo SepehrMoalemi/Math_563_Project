f1f5037d481b8614751b496afb2eecb2d27ee14e
sepehr moalemi <64672710+SepehrMoalemi@users.noreply.github.com>
2023-04-11 11:58:17 -0400

    Add files via upload

--- Code/+salsa/+algorithms/douglasrachfordprimal.m
+++ Code/+salsa/+algorithms/douglasrachfordprimal.m
@@ -1,5 +1,13 @@
 %% Purpose: Primal Douglas-Rachford Splitting
 function [xk, rel_err] = douglasrachfordprimal(prox_tf, prox_g, x, b, i)
+    arguments
+        prox_tf function_handle
+        prox_g  function_handle
+        x       struct
+        b       double
+        i       struct
+    end
+
     %% Get fft Transformations
     [f_A, f_A_T, ~, f_inv_I_ATA] = salsa.fft.get_transformations(i.kernel, b);
 
@@ -17,8 +25,19 @@ function [xk, rel_err] = douglasrachfordprimal(prox_tf, prox_g, x, b, i)
     %% Proximal Operator
     prox_tg = @(x) prox_g(x, t);
 
-    fprintf('stepsize t = %G\n', t);
-    fprintf('==================================\n')
+    %% Relative Error Calculations
+    if isfield(x, 'x_original')
+        f_err = @(xk, xk_old) norm(x.x_original - xk)/norm(x.x_original);
+    else
+        f_err = @(xk, xk_old) norm(xk_old - xk)/norm(xk_old);
+    end
+
+    %% Print Params in use
+    if i.verbos
+        fprintf('stepsize of t = %G, rho = %G.\n', t, rho);
+        fprintf('==================================\n');
+        fprintf('Using Rel_Error = ||xk - xk_1||/||xk_1||\n');
+    end
 
     %% Time Code
     tic
@@ -29,14 +48,12 @@ function [xk, rel_err] = douglasrachfordprimal(prox_tf, prox_g, x, b, i)
 
     indx = 1;
     rel_err = zeros(floor(maxIter/sample_rate),1);
-    
-    fprintf('Using Rel_Error = ||xk - xk_1||/||xk_1||\n')
 
     for k = 1:maxIter
-        if mod(k, sample_rate) == 0
+        if mod(k, sample_rate) == 0 && i.verbos
             time = toc - time;
             fprintf('[%d/%d]-[%G Sec/Iter]: ', k, maxIter,time/k);
-            iter_rel_err = norm(xk_old - xk)/norm(xk_old);
+            iter_rel_err = f_err(xk, xk_old);
             fprintf('Rel_Err = %0.2E\n',iter_rel_err);
             rel_err(indx) = iter_rel_err;
             indx = indx + 1;
@@ -52,5 +69,8 @@ function [xk, rel_err] = douglasrachfordprimal(prox_tf, prox_g, x, b, i)
         z1 = z1 + rho*(uk - xk);
         z2 = z2 + rho*(vk - yk);
     end
-    fprintf('Total Elapsed Time: %f\n', toc);
+    xk = prox_tf(z1);
+    if i.verbos
+        fprintf('Total Elapsed Time: %f\n', toc);
+    end
 end
