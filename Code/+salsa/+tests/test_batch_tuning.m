07469859f95ca9d3b5220a3bb9bbee8a1e810f1d
sepehr moalemi <64672710+SepehrMoalemi@users.noreply.github.com>
2023-04-13 16:20:15 -0400

    added plot overlayer

--- Code/+salsa/+tests/test_batch_tuning.m
+++ Code/+salsa/+tests/test_batch_tuning.m
@@ -1,14 +1,14 @@
 %% Purpose: Batch test tuning parameters
-function test_batch_tuning(file_path, algorithm, ...
-                            problems, gammal1s, gammal2s, ...
-                            arg1s, arg2s, ...
-                            maxIters, sample_rate, ...
-                            blur_type, blur_arg, ...
-                            noise_type, noise_arg, ...
-                            pad_type)
+function test_batch_tuning(file_path, algorithms, ...
+                           problems, gammal1s, gammal2s, ...
+                           arg1s, arg2s, ...
+                           maxIters, sample_rate, ...
+                           blur_type, blur_arg, ...
+                           noise_type, noise_arg, ...
+                           pad_type)
     arguments
         file_path char {mustBeFile(file_path)}
-        algorithm string {mustBeMember(algorithm, ...
+        algorithms string {mustBeMember(algorithms, ...
                                         ["douglasrachfordprimal", ...
                                          "douglasrachfordprimaldual", ...
                                          "admm", ...
@@ -49,88 +49,177 @@ function test_batch_tuning(file_path, algorithm, ...
     %% Set Printing Rate
     i.sample_rate = sample_rate;
 
-    %% Choose Norm Type
-    for problem = problems
-        for indx = 1:length(gammal1s)
-            % Set Problem Smoothing Params
-            i.gammal1 = gammal1s(indx);
-            i.gammal2 = gammal2s(indx);
+    %% Figure Merger
+    % Choose arg2 legend name based on alg
+    arg2_name = "rho";
+    if algorithms(1) == "chambollepock"
+        arg2_name = "s";
+    end
 
-            switch problem
-                case 'l1'
-                    gamma = i.gammal1;
+    % Abbreviation of algorithms for legend
+    algorithm_names = [];
+    for algorithm = algorithms
+        switch algorithm
+            case "douglasrachfordprimal"
+                algorithm_names = [algorithm_names, "primal DR"];
 
-                case 'l2'
-                    gamma = i.gammal2;
+            case "douglasrachfordprimaldual"
+                algorithm_names = [algorithm_names, "primal-dual DR"];
+
+            case "admm"
+                algorithm_names = [algorithm_names, "ADMM"];
+
+            case "chambollepock"
+                algorithm_names = [algorithm_names, "CP"];
+        end
+    end
+
+    params      = {algorithm_names, problems,     gammal1s, gammal2s, arg1s, arg2s};
+    param_names = {algorithm_names, ["l1", "l2"], "\gamma", "\gamma", "t,",  arg2_name};
+    param_len = length(params);
+
+    % Make Legend
+    mode = -1;
+    for cell=1:param_len
+        grid_len = length(params{cell});
+        if grid_len > 1
+            mode = mode + 1;
+            if mode == 1
+                break
             end
+            if cell == 1 || cell == 2
+                legends = param_names{cell};
+            else
+                legends = param_names{cell} + " = " + string(params{cell});
+            end
+        end
+    end
 
-            % Set Optimization Algorithm Param
-            for maxiter = maxIters
-                i.maxiter = maxiter;
-                for arg1 = arg1s
-                    for arg2 = arg2s
-                        %% Set Initial Conditions
-                        switch algorithm
-                            case "douglasrachfordprimal"
-                                i.tprimaldr   = arg1;
-                                i.rhoprimaldr = arg2;
-                                arg1_name = "tprimaldr";
-                                arg2_name = "rhoprimaldr";
-                                x_init.z1 = b;
-                                x_init.z2 = zeros(m,n,3);
+    % Setup Storing figures
+    fig_num = 0;
+    fig_collection = gobjects([2,length(legends)]);
 
+    %% Iterate Over Grid
+    for algorithm = algorithms
+        for problem = problems
+                switch problem
+                    case 'l1'
+                        gammas = gammal1s;
+    
+                    case 'l2'
+                        gammas = gammal2s;
+                end
+            for gamma = gammas
+                % Set Problem Smoothing Params
+                i.gammal1 = gamma;
+                i.gammal2 = gamma;
+    
+                % Set Optimization Algorithm Param
+                for maxiter = maxIters
+                    i.maxiter = maxiter;
+                    for arg1 = arg1s
+                        for arg2 = arg2s
+                            %% Set Initial Conditions
+                            switch algorithm
+                                case "douglasrachfordprimal"
+                                    i.tprimaldr   = arg1;
+                                    i.rhoprimaldr = arg2;
+                                    arg1_name = "tprimaldr";
+                                    arg2_name = "rhoprimaldr";
+                                    x_init.z1 = b;
+                                    x_init.z2 = zeros(m,n,3);
+    
+    
+                                case "douglasrachfordprimaldual"
+                                    i.tprimaldualdr   = arg1;
+                                    i.rhoprimaldualdr = arg2;
+                                    arg1_name = "tprimaldualdr";
+                                    arg2_name = "rhoprimaldualdr";
+                                    x_init.p0 = b;
+                                    x_init.q0 = zeros(m,n,3);
+    
+                                case "admm"
+                                    i.tadmm   = arg1;
+                                    i.rhoadmm = arg2;
+                                    arg1_name = "tadmm";
+                                    arg2_name = "rhoadmm";
+                                    x_init.x0 = b;
+                                    x_init.y0 = zeros(m,n,3);
+                                    x_init.z0 = zeros(m,n,3);
+                                    x_init.u0 = b;
+                                    x_init.w0 = b;
+    
+                                case "chambollepock"
+                                    i.tcp = arg1;
+                                    i.scp = arg2;
+                                    arg1_name = "tcp";
+                                    arg2_name = "scp";
+                                    x_init.x0 = b;
+                                    x_init.y0 = zeros(m,n,3);
+                                    x_init.z0 = b;
+                            end
+    
+                            plt_name =  problem + ...
+                                        "_maxiter_" + num2str(maxiter)+ ...
+                                        "_gamma_" + num2str(gamma)+ ...
+                                        arg1_name + num2str(arg1)+ ...
+                                        arg2_name + num2str(arg2)+"_";
+    
+                            % Run Algorithm
+                            x_out = salsa.solver(problem, algorithm, x_init, kernel, b, i);
+                            
+                            dir_res_chamb = "./Results/" + algorithm + "/";
+                            salsa.util.mkdir_if_no_dir(dir_res_chamb)
 
-                            case "douglasrachfordprimaldual"
-                                i.tprimaldualdr   = arg1;
-                                i.rhoprimaldualdr = arg2;
-                                arg1_name = "tprimaldualdr";
-                                arg2_name = "rhoprimaldualdr";
-                                x_init.p0 = b;
-                                x_init.q0 = zeros(m,n,3);
-
-                            case "admm"
-                                i.tadmm   = arg1;
-                                i.rhoadmm = arg2;
-                                arg1_name = "tadmm";
-                                arg2_name = "rhoadmm";
-                                x_init.x0 = b;
-                                x_init.y0 = zeros(m,n,3);
-                                x_init.z0 = zeros(m,n,3);
-                                x_init.u0 = b;
-                                x_init.w0 = b;
-
-                            case "chambollepock"
-                                i.tcp = arg1;
-                                i.scp = arg2;
-                                arg1_name = "tcp";
-                                arg2_name = "scp";
-                                x_init.x0 = b;
-                                x_init.y0 = zeros(m,n,3);
-                                x_init.z0 = b;
+                            % Store Figures
+                            figs = findobj('Type', 'figure');
+                            fig_num = fig_num + 1;
+                            fig_collection(:,fig_num) = figs(1:2);
+    
+                            % Plot Deblurred Image
+                            fig = figure('Name',"Deblurred Image #" + num2str(fig_num) );
+                            imshow(x_out,[])
+                            saveas(fig,dir_res_chamb+plt_name+".png")
+                            
+                            if mode == 1
+                                close all;
+                            end
                         end
-
-                        plt_name =  problem + ...
-                                    "_maxiter_" + num2str(maxiter)+ ...
-                                    "_gamma_" + num2str(gamma)+ ...
-                                    arg1_name + num2str(arg1)+ ...
-                                    arg2_name + num2str(arg2)+"_";
-
-                        % Run Algorithm
-                        x_out = salsa.solver(problem, algorithm, x_init, kernel, b, i);
-                        
-                        dir_res_chamb = "./Results/" + algorithm + "/";
-                        salsa.util.mkdir_if_no_dir(dir_res_chamb)
-                        saveas(gcf,dir_res_chamb+"err_"+plt_name+".png")
-
-                        % Plot Deblurred Image
-                        fig = figure('Name','Deblurred Image' );
-                        imshow(x_out,[])
-                        saveas(fig,dir_res_chamb+plt_name+".png")
-
-                        close all
                     end
                 end
             end
         end
     end
+
+    %% Plot Merged Figures
+    fig_rel_err = fig_collection(1,:);
+    fig_rel_obj = fig_collection(2,:);
+    
+    handleLine = findobj(fig_rel_err,'type','line');
+    merged_fig = figure('Name',"Relative Error");
+    ax = axes(merged_fig);
+    hold on;
+    for indx = 1:length(handleLine)
+        line = handleLine(indx);
+        plot(get(line,'XData'), get(line,'YData'), '--') ;
+    end
+    title(ax,'||x^k - x^*||/||x^*||')
+    xlabel('Iteration Number'); ylabel('Relative Error')
+    box('on');grid('on');
+    legend(legends);
+    set(ax, 'Yscale','log', 'FontSize',14)
+
+    handleLine = findobj(fig_rel_obj,'type','line');
+    merged_fig = figure('Name',"Optimality gap");
+    ax = axes(merged_fig);
+    hold on;
+    for indx = 1:length(handleLine)
+        line = handleLine(indx);
+        plot(get(line,'XData'), get(line,'YData'), '--') ;
+    end
+    title(ax,'(f(x^k) - f(x^*))/f(x^*)')
+    xlabel(ax,'Iteration Number'); ylabel(ax,'Relative Objective Value')
+    box('on');grid('on');
+    legend(legends);
+    set(ax, 'Yscale','log', 'FontSize',14)
 end
